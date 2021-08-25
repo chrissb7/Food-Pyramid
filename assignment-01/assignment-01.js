cb97e7b8575f616cbb6c0e18a80e17f0ab46d14a
chrissb7 <74411730+chrissb7@users.noreply.github.com>
2021-08-25 15:42:55 +0100

    Simple Food Pyramid

---
+++ assignment-01/assignment-01.js
@@ -0,0 +1,98 @@
+
+    function addOne(){
+        var add = document.getElementById('number').innerHTML;
+        add++;
+        document.getElementById('number').innerHTML = add;
+    }
+    
+    function minusOne(){
+        var minus = document.getElementById('number').innerHTML;   
+        
+        if(minus!='0'){
+        minus--;
+        document.getElementById('number').innerHTML = minus;
+        }
+    }
+    
+    function addOne2(){
+        var add = document.getElementById('number2').innerHTML;
+        add++;
+        document.getElementById('number2').innerHTML = add;
+    }
+    
+    function minusOne2(){
+        var minus = document.getElementById('number2').innerHTML;   
+        
+        if(minus!='0'){
+        minus--;
+        document.getElementById('number2').innerHTML = minus;
+        }
+    }
+    
+    function addOne3(){
+        var add = document.getElementById('number3').innerHTML;
+        add++;
+        document.getElementById('number3').innerHTML = add;
+    }
+    
+    function minusOne3(){
+        var minus = document.getElementById('number3').innerHTML;   
+        
+        if(minus!='0'){
+        minus--;
+        document.getElementById('number3').innerHTML = minus;
+        }
+    }
+    function addOne4(){
+        var add = document.getElementById('number4').innerHTML;
+        add++;
+        document.getElementById('number4').innerHTML = add;
+    }
+    
+    function minusOne4(){
+        var minus = document.getElementById('number4').innerHTML;   
+        
+        if(minus!='0'){
+        minus--;
+        document.getElementById('number4').innerHTML = minus;
+        }
+    }
+    
+    function addOne5(){
+        var add = document.getElementById('number5').innerHTML;
+        add++;
+        document.getElementById('number5').innerHTML = add;
+    }
+    
+    function minusOne5(){
+        var minus = document.getElementById('number5').innerHTML;   
+        
+        if(minus!='0'){
+        minus--;
+        document.getElementById('number5').innerHTML = minus;
+        }
+    }
+    function addOne6(){
+        var add = document.getElementById('number6').innerHTML;
+        add++;
+        document.getElementById('number6').innerHTML = add;
+    }
+    
+    function minusOne6(){
+        var minus = document.getElementById('number6').innerHTML;   
+        
+        if(minus!='0'){
+        minus--;
+        document.getElementById('number6').innerHTML = minus;
+        }
+    }
+    
+    
+    
+    function getSelectValue()
+    {
+        var selectedValue= document.getElementById("list").value;
+        console.log(selectedValue);
+    }
+    
+    
